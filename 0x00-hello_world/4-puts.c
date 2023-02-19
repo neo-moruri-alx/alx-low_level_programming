c980425e175438390e1f8d52c7da437edafb834c
Neo Moruri <Neo.Moruri@nwu.ac.za>
2023-02-19 12:00:43 -0800

    Trial 5 - phase 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,13 @@
 #!/bin/bash
-#!/bin/bash
-#!/bin/bash
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like buidling a multilingual puzzle");
+return (0);
+}
