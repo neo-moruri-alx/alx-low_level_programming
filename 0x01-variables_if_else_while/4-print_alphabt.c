a126a62bb15a3c0f150e486b5f02318c150ab6c3
Neo Moruri <124310852+neo-moruri-alx@users.noreply.github.com>
2023-03-05 16:34:10 +0200

    Betty coding style - rechecked

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,6 +12,7 @@ int main(void)
 	char low, e, q;
 	e = 'e';
 	q = 'q';
+
 	for (low = 'a'; low <= 'z'; low++)
 	{
 		if (low != e && low != q)
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -12,6 +12,7 @@
 int main(void)
 {
 	char low;
+
 	for (low = 'z'; low >= 'a'; low--)
 		putchar(low);
 		putchar('\n');
